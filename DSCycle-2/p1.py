4a070f6db78b553bae5d315b8a6f8d72de46a4a5
bhanumatimohan <74654683+bhanumatimohan@users.noreply.github.com>
2021-11-29 10:28:11 +0530

    Add files via upload

---
+++ DSCycle-2/p1.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+#1. Create a three dimensional array specifying float data type and print it.
+
+import numpy as np
+
+array_3d=np.array([[[1,2],[3,4]],[[5,6],[7,8]]], dtype='f')
+print(array_3d)
+print(array_3d.dtype)

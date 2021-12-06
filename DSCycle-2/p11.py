c277a32730cd0bb5dfa1026b9a0e1412fb0552f1
bhanumatimohan <74654683+bhanumatimohan@users.noreply.github.com>
2021-12-06 11:20:51 +0530

    Add files via upload

---
+++ DSCycle-2/p11.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec  6 10:30:16 2021
+
+@author: sjcet
+"""
+
+#11. Demonstarte the use of sum() function in 1D and 2D array.
+
+import numpy as np
+
+array_1d=np.array([1,2,3,4,5])
+x=np.sum(array_1d)
+print(array_1d)
+print("the use of sum() function in 1D array",x)
+
+array_2d=np.array([[[1,2,3],[4,5,6],[7,8,9]]])
+y=np.sum(array_2d)
+print("\n",array_2d)
+print("the use of sum() function in 1D array",y)
